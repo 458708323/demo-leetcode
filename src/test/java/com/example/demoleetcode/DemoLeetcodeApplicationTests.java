1af6bf878a1be6c49c7add963fd06b22e64f8ec5
zhaohui <zhaohui@yupaopao.cn>
2019-06-19 19:32:24 +0800

    Initial commit

---
+++ src/test/java/com/example/demoleetcode/DemoLeetcodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoleetcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoLeetcodeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
