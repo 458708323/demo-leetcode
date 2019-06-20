39cbee00b13e35c4544c73036e338b9ea77baddd
zhaohui <zhaohui@yupaopao.cn>
2019-06-20 10:20:59 +0800

    feat

--- src/main/java/com/example/demoleetcode/leetcode/StringInversion.java
+++ src/main/java/com/example/demoleetcode/leetcode/StringInversion.java
@@ -1,11 +1,11 @@
 package com.example.demoleetcode.leetcode;
 
-import com.alibaba.fastjson.JSON;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Description:
+ * 字符串反转
  * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
  * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
  * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
@@ -15,15 +15,15 @@ import java.util.List;
  * @author zhaohui
  * @create 2019/6/19 7:19 PM
  */
-public class Solution {
+public class StringInversion {
 
     public void reverseString(char[] s) {
         s = getSortChar(s, 0, s.length - 1);
-        List<String> a = new ArrayList<>();
-        for (char ss : s) {
-            a.add(String.valueOf(ss));
+        List<String> list = new ArrayList<>();
+        for (char sChar : s) {
+            list.add(String.valueOf(sChar));
         }
-        System.out.println(a.toString());
+        System.out.println(list.toString());
     }
 
     public char[] getSortChar(char[] s, int a, int b) {
@@ -39,9 +39,9 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        StringInversion stringInversion = new StringInversion();
         char[] s = "hello".toCharArray();
-        solution.reverseString(s);
+        stringInversion.reverseString(s);
     }
 
 }
